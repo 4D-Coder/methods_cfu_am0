4e7674c255c47abebbcf1d05ca2fa657d27206e9
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-21 18:35:31 -0600

    Complete final_practice.rb question 2

--- final_practice.rb
+++ final_practice.rb
@@ -10,20 +10,19 @@ puts greeting # => "Hello friend how are you today?"
 # How many arguments did you pass your method?
 # One argument
 
-
-
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting(name)
+    "Hello #{name}, how are you today?"
+end  
 
 # What is the return value of your method?
+puts custom_greeting("Jessica") # => "Hello Jessica, how are you today?"
 
 # How many arguments did you pass your method?
 # One
 
 # What data type was your argument(s)?
 # A String
-
-
-
 
 #3: Write a method named square that takes in one number, and returns the square of that number
 
