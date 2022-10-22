151d7ebde6edda4d242f5b0ccb78556471d34940
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-21 18:34:08 -0600

    Complete final_practice.rb question 1

--- final_practice.rb
+++ final_practice.rb
@@ -1,18 +1,26 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+    "Hello friend, how are you today?"
+end  
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
+puts greeting # => "Hello friend how are you today?"
 
+# How many arguments did you pass your method?
+# One argument
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
-
 # What is the return value of your method?
+
 # How many arguments did you pass your method?
+# One
+
 # What data type was your argument(s)?
+# A String
 
 
 
